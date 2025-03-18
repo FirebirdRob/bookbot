bd49608aa93b5411cbc53f6b1715fb808919a5ad
FirebirdRob <Firebird.Robin@gmail.com>
2025-03-18 16:09:04 +0000

    new books added

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from stats import get_num_words, sort_on, get_chars_dict
-
+import sys
 
 
 def main():
 
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
     chars_dict = get_chars_dict(text)
     sorted_items = sort_on(chars_dict)
-    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"--- Begin report of {book_path} ---")
 
     for char, freq in sorted_items:
         print(f"The '{char}' character was found {freq} times")
